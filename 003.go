c8de2fbe92c6a0219930495c43237a5fdb76dacf
Joel Mueller <joel.mueller@gmail.com>
2017-07-23 01:25:45 -0500

    Added menu, timing

--- 003.go
+++ 003.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+func isPrime(n int) bool {
+	switch {
+	case n <= 1:
+		return false
+	case n == 2 || n == 3:
+		return true
+	case n%2 == 0 || n%3 == 0:
+		return false
+	default:
+		i, w := 5, 2
+		for i*i <= n {
+			if n%i == 0 {
+				return false
+			}
+			i += w
+			w = 6 - w
+		}
+		return true
+	}
+}
+
+func largestPrimeFactor(n uint64) uint64 {
+	x := n
+	var fact uint64
+
+	for i := uint64(2); i*i <= x; {
+		switch {
+		case x%i == 0:
+			x = x / i
+			fact = i
+		case i == 2:
+			i = 3
+		default:
+			i += 2
+		}
+	}
+	if x > fact {
+		// the remainder is a prime number
+		fact = x
+	}
+
+	return fact
+}
 
 func run003() {
 	fmt.Print("003: ")
-
+	fmt.Println(largestPrimeFactor(600851475143))
 }
--- euler.go
+++ euler.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Euler...")
-	run001()
-	run002()
-	run003()
+	problems := []func(){
+		run001, run002, run003,
+	}
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("\nEnter Euler problem number, up to %d: ", len(problems))
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimRight(input, "\r\n")
+		if input == "exit" {
+			break
+		}
+
+		num, err := strconv.Atoi(input)
+		if err == nil {
+			start := time.Now()
+			problems[num-1]()
+			elapsed := time.Since(start)
+			fmt.Printf("Execution time: %s\n", elapsed)
+		}
+	}
 }
